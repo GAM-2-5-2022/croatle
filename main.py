d0f7175d0faee28cdbddaf25e56381e267a6878d
rzenz <105455549+rzenz@users.noreply.github.com>
2022-05-20 13:40:00 +0200

    GUI naslov, slike

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from tkinter.ttk import Label
+from PIL import Image, ImageTk
+ 
 
-gui = Tk(className='Python Examples - Window Size')
-gui.geometry("720x480")
-frm = ttk.Frame(gui, padding=10)
-frm.grid()
-ttk.Label(frm, text="Croatle").grid(column=0, row=0)
+gui = tk.Tk()
+gui.geometry("1024x768")
+gui.wm_title('Croatle')
+title = Label(gui, text='Croatle', font=('Segoe UI', 25, 'bold'))
+title.pack(ipadx=10, ipady=10)
 
-gui.mainloop() 
+img= Image.open("images/test.png")
+resize_img = img.resize((400,300), Image.ANTIALIAS)
+image_zup = ImageTk.PhotoImage(resize_img)
+
+
+img_zup = tk.Label(gui, image = image_zup)
+img_zup.pack()
+gui.mainloop()
